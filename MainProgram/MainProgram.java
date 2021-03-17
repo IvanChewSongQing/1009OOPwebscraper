b2c365647fad3635278aecef4b8f429a5204696a
IvanChewSongQing <78472959+IvanChewSongQing@users.noreply.github.com>
2021-03-18 00:16:05 +0800

    Update MainProgram.java

--- MainProgram/MainProgram.java
+++ MainProgram/MainProgram.java
@@ -1,9 +1,21 @@
-package MainProgram;
 public class MainProgram {
 	
+	public void crawl(Crawler crawler) {
+		crawler.crawl();
+	}
+	
+	public void store(Crawler crawler) {
+		crawler.store();
+	}
+	
 	public static void main(String args[]) {
-		TwitterCrawler twitter = new TwitterCrawler();
-		twitter.crawl();
+		MainProgram program = new MainProgram();
+		Crawler twitter = new TwitterCrawlerV2();
+		//RedditCrawler reddit = new RedditCrawler();
+		program.crawl(twitter);
+		//program.crawl(reddit);
+		program.store(twitter);
+		//program.store(reddit);
 	}
 	
 }
